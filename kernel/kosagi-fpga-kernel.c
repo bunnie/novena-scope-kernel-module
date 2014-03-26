abe62636984db0e18226aa1e6c2379a409f39ad5
Sean Cross <xobs@kosagi.com>
2014-03-26 13:31:53 +0800

    kernel: Initial attempt to read data

--- kernel/kosagi-fpga-kernel.c
+++ kernel/kosagi-fpga-kernel.c
@@ -3,11 +3,58 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/dmaengine.h>
+#include <linux/delay.h>
 
 #include <linux/platform_data/dma-imx.h>
 
+#define IMX6_EIM_CS0_BASE_ADDR 0x08040000
 #define IMX6_EIM_CS1_BASE_ADDR 0x0c040000
 #define DATA_FIFO_ADDR (IMX6_EIM_CS1_BASE_ADDR + 0xf000)
+
+#define FPGA_W_TEST0       0
+#define FPGA_W_TEST1       1
+
+#define FPGA_W_DDR3_P2_CMD  16
+#define FPGA_W_DDR3_P2_LADR 17
+#define FPGA_W_DDR3_P2_HADR 18
+#define FPGA_W_DDR3_P2_WEN  19
+#define FPGA_W_DDR3_P2_LDAT 20
+#define FPGA_W_DDR3_P2_HDAT 21
+
+#define FPGA_W_DDR3_P3_CMD  24
+#define FPGA_W_DDR3_P3_LADR 25
+#define FPGA_W_DDR3_P3_HADR 26
+#define FPGA_W_DDR3_P3_REN  27
+
+#define FPGA_W_ADC_CTL       128
+#define FPGA_W_ADC_SAMPLEN_L 129
+#define FPGA_W_ADC_SAMPLEN_H 130
+
+#define FPGA_W_RBK_CTL       136
+#define  RBK_CTL_CLEAR_ERROR 0x4
+#define  RBK_CTL_INIT        0x2
+#define  RBK_CTL_ENABLE      0x1
+#define FPGA_W_RBK_PAGE_L    137
+#define FPGA_W_RBK_PAGE_H    138
+#define FPGA_W_BURSTLEN      139
+
+
+#define FPGA_R_TEST0        2048
+#define FPGA_R_TEST1        2049
+#define FPGA_R_DDR3_CAL     2050
+
+#define FPGA_R_DDR3_P2_STAT 2064
+#define FPGA_R_DDR3_P3_STAT 2072
+#define FPGA_R_DDR3_P3_LDAT 2073
+#define FPGA_R_DDR3_P3_HDAT 2074
+
+#define FPGA_R_ADC_STAT     2176
+#define FPGA_R_RBK_STAT     2177
+#define FPGA_R_MEAS_BURST   2178
+          
+#define FPGA_R_DDR3_V_MINOR 4094
+#define FPGA_R_DDR3_V_MAJOR 4095
+
 
 /* list of valid commands */
 enum kosagi_fpga_commands {
@@ -29,7 +76,8 @@ enum kosagi_fpga_attributes {
 
 struct kosagi_fpga {
 	unsigned int seq_num;
-	void *byte_area;
+	__iomem void *byte_area;
+	__iomem u16 *fpga_ctrl;
 	unsigned int byte_size;
 	u8 dma_buffer[4096];
 };
@@ -91,12 +139,45 @@ out:
 	return 0;
 }
 
+static void kosagi_trigger_sample(void)
+{
+	fpga->fpga_ctrl[FPGA_W_ADC_SAMPLEN_L] = 0x0;
+	fpga->fpga_ctrl[FPGA_W_ADC_SAMPLEN_H] = 0x1; // capture 64k for now
+
+	fpga->fpga_ctrl[FPGA_W_ADC_CTL] = 0x0;
+	fpga->fpga_ctrl[FPGA_W_ADC_CTL] = 0x2;
+	fpga->fpga_ctrl[FPGA_W_ADC_CTL] = 0x0;
+	udelay(1000);
+	fpga->fpga_ctrl[FPGA_W_ADC_CTL] = 0x1; // start sampling run, fill buffer
+}
+
+static void kosagi_trigger_burst(void)
+{
+	/* Set burst mode priority */
+	fpga->fpga_ctrl[FPGA_W_DDR3_P3_CMD] |= 0x8000;
+
+	pr_info("last run's burst length was: %d\n",
+			fpga->fpga_ctrl[FPGA_R_MEAS_BURST]);
+
+	fpga->fpga_ctrl[FPGA_W_RBK_PAGE_L] = 0x0;
+	fpga->fpga_ctrl[FPGA_W_RBK_PAGE_H] = 0x0; // read back starting at page 0
+
+	fpga->fpga_ctrl[FPGA_W_BURSTLEN] = 0x10; // 16-beat bursts
+
+	fpga->fpga_ctrl[FPGA_W_RBK_CTL] = RBK_CTL_CLEAR_ERROR | RBK_CTL_INIT;
+	fpga->fpga_ctrl[FPGA_W_RBK_CTL] = 0;
+	fpga->fpga_ctrl[FPGA_W_RBK_CTL] = RBK_CTL_ENABLE; // enable readback machine
+}
+
 static int kosagi_fpga_read(struct sk_buff *skb_2, struct genl_info *info)
 {
 	struct sk_buff *buf;
 	void *hdr = NULL;
 	void *data;
 	int ret = 0;
+
+	kosagi_trigger_sample();
+	kosagi_trigger_burst();
 
 	if (info == NULL) {
 		pr_err("Unable to read: info was NULL\n");
@@ -181,10 +262,18 @@ static int __init kosagi_fpga_init(void)
 	if (!fpga)
 		goto fail;
 
+	fpga->fpga_ctrl = ioremap(IMX6_EIM_CS0_BASE_ADDR, 4096);
+	pr_err("Scope: Remapped data 0x%08x to 0x%p\n",
+			IMX6_EIM_CS0_BASE_ADDR, fpga->fpga_ctrl);
+
 	fpga->byte_area = ioremap_wc(DATA_FIFO_ADDR, 4096);
-	pr_err("Scope: Remapped 0x%08x to 0x%p\n",
+	pr_err("Scope: Remapped data 0x%08x to 0x%p\n",
 			DATA_FIFO_ADDR, fpga->byte_area);
 	fpga->byte_size = 4096;
+
+	pr_err("Scope: FPGA version %d.%d\n",
+			fpga->fpga_ctrl[FPGA_R_DDR3_V_MAJOR],
+			fpga->fpga_ctrl[FPGA_R_DDR3_V_MINOR]);
 
 	return 0;
 	
@@ -201,6 +290,12 @@ static void __exit kosagi_fpga_exit(void)
 		pr_err("Scope: Unmapped 0x%p from 0x%08x\n",
 				fpga->byte_area, DATA_FIFO_ADDR);
 		iounmap(fpga->byte_area);
+	}
+
+	if (fpga->fpga_ctrl) {
+		pr_err("Scope: Unmapped 0x%p from 0x%08x\n",
+				fpga->fpga_ctrl, IMX6_EIM_CS0_BASE_ADDR);
+		iounmap(fpga->fpga_ctrl);
 	}
 
 	ret = genl_unregister_family(&kosagi_fpga_family);
